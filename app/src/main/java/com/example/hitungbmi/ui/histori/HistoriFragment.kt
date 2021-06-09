8b39076e66b625216904bcb0f8a3b108b5b09874
davidsryansa <ryanzadavid>
2021-06-09 08:28:35 -0700

    Menghapus data dari database

--- app/src/main/java/com/example/hitungbmi/ui/histori/HistoriFragment.kt
+++ app/src/main/java/com/example/hitungbmi/ui/histori/HistoriFragment.kt
@@ -11,9 +11,11 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
+import com.example.hitungbmi.R
 import com.example.hitungbmi.databinding.FragmentHistoriBinding
 import com.example.hitungbmi.db.BmiDb
 import com.example.hitungbmi.db.BmiEntity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 
 class HistoriFragment : Fragment() {
@@ -39,7 +41,9 @@ class HistoriFragment : Fragment() {
                 RecyclerView.VERTICAL)
             )
             adapter = myAdapter
-            setHasFixedSize(true) }
+            setHasFixedSize(true)
+        }
+        setHasOptionsMenu(true)
         return binding.root
     }
 
@@ -51,5 +55,26 @@ class HistoriFragment : Fragment() {
                     View.VISIBLE else View.GONE
             myAdapter.updateData(it as List<BmiEntity>)
         })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater.inflate(R.menu.histori_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_hapus) { hapusData()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    } private fun hapusData() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setMessage(R.string.konfirmasi_hapus)
+            .setPositiveButton(getString(R.string.hapus)) { _, _ ->
+                viewModel.hapusData() }
+            .setNegativeButton(getString(R.string.batal)) {dialog, _ ->
+                dialog.cancel()
+            } .show()
     }
 }
--- app/src/main/java/com/example/hitungbmi/ui/histori/HistoriViewModel.kt
+++ app/src/main/java/com/example/hitungbmi/ui/histori/HistoriViewModel.kt
@@ -1,8 +1,15 @@
 package com.example.hitungbmi.ui.histori
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.hitungbmi.db.BmiDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class HistoriViewModel(db: BmiDao) : ViewModel() {
+class HistoriViewModel(private val db: BmiDao) : ViewModel() {
     val data = db.getLastBmi()
+    fun hapusData() = viewModelScope.launch {
+        withContext(Dispatchers.IO) {
+            db.clearData() } }
 }
